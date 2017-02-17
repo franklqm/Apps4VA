e565ae88667dd0e990d5fe52ec22a3af145fe8d0
Jeffrey <antetojl@dukes.jmu.edu>
2017-02-16 20:05:49 -0500

    Created tables

--- schema/create.sql
+++ schema/create.sql
@@ -1,59 +1,56 @@
-DROP TABLE IF EXISTS division;
+CREATE TABLE teacher_table (
+	div_num integer NOT NULL,
+	div_name text NOT NULL,
+	elementary_salary real NOT NULL, 
+	secondary_salary real NOT NULL, 
+	average_salary real NOT NULL);
+	
+ALTER TABLE teacher_table OWNER TO crew;
 
-CREATE TABLE division (
-    div_num integer NOT NULL,
-    div_name text NOT NULL
-);
-
-ALTER TABLE division OWNER TO absent;
-
-COMMENT ON TABLE division IS 'all divisions in Virginia';
-
-
-DROP TABLE IF EXISTS school;
-
-CREATE TABLE school (
-    div_num integer NOT NULL,
-    sch_num integer NOT NULL,
-    sch_name text NOT NULL
-);
-
-ALTER TABLE school OWNER TO absent;
-
-COMMENT ON TABLE school IS 'all schools in Virginia';
-
-
-DROP TABLE IF EXISTS enroll;
-
-CREATE TABLE enroll (
-    sch_year integer NOT NULL,
-    div_num integer NOT NULL,
-    sch_num integer NOT NULL,
-    race text NOT NULL,
-    gender text NOT NULL,
-    disabil text NOT NULL,
-    lep text NOT NULL,
+COMMENT ON TABLE teacher_table IS 'Teacher salaries in Virginia';		
+	
+CREATE TABLE absentee_table(
+	sch_year integer NOT NULL,
+	div_num integer NOT NULL,
+	div_name text NOT NULL,
+	gender text NOT NULL,
+	race text NOT NULL, 
+	disabil text NOT NULL,
+   	lep text NOT NULL,
     disadva text NOT NULL,
-    grade_num integer NOT NULL,
-    fall_cnt integer NOT NULL
-);
+	grade_num integer NOT NULL, 
+	absentee_percentage real NOT NULL);
+	
+ALTER TABLE absentee_table OWNER TO crew;
 
-ALTER TABLE enroll OWNER TO absent;
+COMMENT ON TABLE absentee_table IS 'absentee data in Virginia';	
 
-COMMENT ON TABLE enroll IS 'fall membership counts';
+CREATE TABLE pass_rate (
+	sch_year integer NOT NULL,
+	div_num integer NOT NULL,
+	div_name text NOT NULL,
+	gender text NOT NULL,
+	race text NOT NULL, 
+	disabil text NOT NULL,
+   	lep text NOT NULL,
+    disadva text NOT NULL,
+	pass_rate real NOT NULL);
+	
+ALTER TABLE pass_rate OWNER TO crew;
 
+COMMENT ON TABLE pass_rate IS 'Four year pass rates in Virginia';
 
-DROP TABLE IF EXISTS college;
+CREATE TABLE secondary_enrollment(
+	sch_year integer NOT NULL,
+	div_num integer NOT NULL,
+	div_name text NOT NULL,
+	gender text NOT NULL,
+	race text NOT NULL, 
+	disabil text NOT NULL,
+   	lep text NOT NULL,
+    disadva text NOT NULL,
+	pass_rate real NOT NULL);
+	
+ALTER TABLE seconday_enrollment OWNER TO crew;
 
-CREATE TABLE college (
-    unitid integer NOT NULL,
-    instnm text NOT NULL,
-    addr text NOT NULL,
-    city text NOT NULL,
-    state text NOT NULL,
-    zip text NOT NULL
-);
-
-ALTER TABLE college OWNER TO absent;
-
-COMMENT ON TABLE college IS 'colleges and universities';
+COMMENT ON TABLE seconday_enrollment IS 'Secondary enrollment in Virginia ';	
