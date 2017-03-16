629de64e4c6d7268e4e7e53d02a076ff6c57720f
Matt Bowyer <mattbowyer16@gmail.com>
2017-03-16 17:03:09 -0400

    Alphabetized table names in copy.sh

--- schema/copy.sh
+++ schema/copy.sh
@@ -1,6 +1,14 @@
 ﻿#!/bin/sh
 # Run this script directly on the db server.
 
+echo COPY pass_rate FROM vdoe
+psql -c "COPY (
+    SELECT o.sch_year, o.div_num, d.div_name, o.gender, o.race, o.disabil, o.lep, o.disadva, o.diploma_rate
+    FROM ontime_cohort o
+	JOIN division d ON o.div_num = d.div_num
+  ) TO STDOUT;" vdoe | \
+  psql -c "COPY pass_rate FROM STDIN;" crew
+
 echo COPY secondary_enrollment FROM vdoe
 psql -c "COPY (
     SELECT p.sch_year, p.div_num, d.div_name, p.gender, p.race, p.disabil, p.lep, p.disadva, ps_enrollment_cnt
@@ -9,12 +17,6 @@ psql -c "COPY (
   ) TO STDOUT;" vdoe | \
   psql -c "COPY secondary_enrollment FROM STDIN;" crew
 
-echo COPY pass_rate FROM vdoe
-psql -c "COPY (
-    SELECT o.sch_year, o.div_num, d.div_name, o.gender, o.race, o.disabil, o.lep, o.disadva, o.diploma_rate
-    FROM ontime_cohort o
-	JOIN division d ON o.div_num = d.div_num
-  ) TO STDOUT;" vdoe | \
-  psql -c "COPY pass_rate FROM STDIN;" crew
+  
 
 
