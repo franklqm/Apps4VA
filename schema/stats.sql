c0480aec372acdcffee892a449ace2e574faa173
Matt Bowyer <mattbowyer16@gmail.com>
2017-03-16 12:22:21 -0400

    Corrected defect in stats.sql for table names (teacher_table to teacher)

--- schema/stats.sql
+++ schema/stats.sql
@@ -1,8 +1,8 @@
 ANALYZE VERBOSE teacher;
-SELECT count(*) AS teach_cnt FROM teacher_table;
+SELECT count(*) AS teach_cnt FROM teacher;
 
 ANALYZE VERBOSE absentee;
-SELECT count(*) AS absent_cnt FROM absentee_table;
+SELECT count(*) AS absent_cnt FROM absentee;
 
 ANALYZE VERBOSE pass_rate;
 SELECT count(*) AS pass_cnt FROM pass_rate;
