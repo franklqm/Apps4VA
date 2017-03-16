6c1c8b5a555be2a5381c42dee3b10b645b20a29f
Matt Bowyer <mattbowyer16@gmail.com>
2017-03-16 17:21:20 -0400

    Changing crew database design to import tables as is from vdoe database (instead of joining vdoe tables to form less cohesive tables in crew); this will remove unnecessary complexity

--- schema/copy.sh
+++ schema/copy.sh
@@ -3,20 +3,14 @@
 
 echo COPY pass_rate FROM vdoe
 psql -c "COPY (
-    SELECT o.sch_year, o.div_num, d.div_name, o.gender, o.race, o.disabil, o.lep, o.disadva, o.diploma_rate
-    FROM ontime_cohort o
-	JOIN division d ON o.div_num = d.div_num
+    SELECT *
+    FROM ontime_cohort
   ) TO STDOUT;" vdoe | \
   psql -c "COPY pass_rate FROM STDIN;" crew
 
 echo COPY secondary_enrollment FROM vdoe
 psql -c "COPY (
-    SELECT p.sch_year, p.div_num, d.div_name, p.gender, p.race, p.disabil, p.lep, p.disadva, ps_enrollment_cnt
-    FROM postsec_enroll p
-	JOIN division d ON p.div_num = d.div_num
+    SELECT *
+    FROM postsec_enroll
   ) TO STDOUT;" vdoe | \
   psql -c "COPY secondary_enrollment FROM STDIN;" crew
-
-  
-
-
