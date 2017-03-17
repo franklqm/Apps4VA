6205a7fd0742a1bab547b019c4b1ba6311c29b9c
Matt Bowyer <mattbowyer16@gmail.com>
2017-03-16 20:37:30 -0400

    Added ANALYZE statements for the division and school tables in stats.sql

--- schema/stats.sql
+++ schema/stats.sql
@@ -1,11 +1,17 @@
-ANALYZE VERBOSE teacher;
-SELECT count(*) AS teach_cnt FROM teacher;
-
 ANALYZE VERBOSE absentee;
 SELECT count(*) AS absent_cnt FROM absentee;
+
+ANALYZE VERBOSE division;
+SELECT count(*) AS div_cnt FROM division;
 
 ANALYZE VERBOSE pass_rate;
 SELECT count(*) AS pass_cnt FROM pass_rate;
 
+ANALYZE VERBOSE school;
+SELECT count(*) AS sch_cnt FROM school;
+
 ANALYZE VERBOSE secondary_enrollment;
 SELECT count(*) AS sec_cnt FROM secondary_enrollment;
+
+ANALYZE VERBOSE teacher;
+SELECT count(*) AS teach_cnt FROM teacher;
