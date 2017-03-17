74755a456340b0f245e372bd0e07c875d7993f0c
Matt Bowyer <mattbowyer16@gmail.com>
2017-03-17 10:19:18 -0400

    Added absent_to_salary.sql in the queries directory

---
+++ queries/absent_to_salary.sql
@@ -0,0 +1,25 @@
+﻿--
+--
+--
+-- 
+
+DROP FUNCTION IF EXISTS absent_to_salary(loc_type text, absentee_percentage real);
+
+CREATE FUNCTION absent_to_salary(loc_type text, absentee_percentage real)
+RETURNS TABLE(div_name text, average_salary real) AS $$ 
+
+	SELECT d.div_name, t.average_salary
+	FROM division d
+		JOIN absentee a ON d.div_num = a.div_num
+		JOIN teacher t ON d.div_num = t.div_num
+	WHERE d.loc_type = $1
+		AND a.absentee_percentage = $2
+		AND p.race = 'ALL'
+		AND p.gender = 'ALL'
+		AND p.disabil = 'ALL'
+		AND p.lep = 'ALL'
+		AND p.disadva = 'ALL'
+
+$$ LANGUAGE SQL STABLE STRICT;
+
+ALTER FUNCTION absent_to_salary(loc_type text, absentee_percentage real) OWNER TO crew;
