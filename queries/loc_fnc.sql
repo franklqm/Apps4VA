5664c7b3467c28ecdb059d7e88d99701ff6b29ca
Matt Bowyer <mattbowyer16@gmail.com>
2017-03-16 23:01:50 -0400

    Corrected defect in pass_rate JOIN clause in loc_fnc.sql

--- queries/loc_fnc.sql
+++ queries/loc_fnc.sql
@@ -11,7 +11,7 @@ RETURNS TABLE(div_name text, absent_percentage real, diploma_rate real) AS $$
 	SELECT d.div_name, a.absentee_percentage, p.diploma_rate
 	FROM division d
 		JOIN absentee a ON d.div_num = a.div_num 
-		JOIN pass_rate p ON d.div_num 
+		JOIN pass_rate p ON d.div_num = p.div_num
 	WHERE d.loc_type = $1
 		AND a.sch_year = $2
 		AND p.sch_year = $2
